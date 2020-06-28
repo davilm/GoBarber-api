430ecf7f2634e81ffdd00026ba338a8fc3065a17
Davi Leal <lealm.davi@gmail.com>
2020-06-28 14:01:16 -0300

    Relationship in Models

--- backend/src/database/migrations/1593180895267-CreateAppointments.ts
+++ backend/src/database/migrations/1593180895267-CreateAppointments.ts
@@ -9,7 +9,7 @@ export class CreateAppointments1593180895267 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
