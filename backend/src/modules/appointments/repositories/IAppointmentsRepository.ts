b80fdd4c4963cbb3299725f020efa2b38379f23b
Davi Leal <lealm.davi@gmail.com>
2020-09-15 22:41:15 -0300

    rewriting repositories

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
-interface IAppointmentsRepository {
-    create(): Promise<Appointment | undefined>
+export default interface IAppointmentsRepository {
+    create(data: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>
 }
