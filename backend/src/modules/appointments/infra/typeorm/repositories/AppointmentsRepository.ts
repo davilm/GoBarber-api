67bcb40f325c6bd0577880d4649c639a38b39b2d
Davi Leal <lealm.davi@gmail.com>
2020-10-06 18:58:32 -0300

    list availability hours

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository, Raw } from 'typeorm';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 import Appointment from '../entities/Appointment';
 
@@ -27,12 +28,34 @@ class AppointmentsRepository implements IAppointmentsRepository {
     }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
         const parsedMonth = String(month).padStart(2, '0');
 
-        const appointmenrs = await this.ormRepository.find({
+        const appointments = await this.ormRepository.find({
             where: {
                 provider_id,
                 date: Raw(
                     dateFieldName =>
                         `to_chart(${dateFieldName}.'MM-YYYY') = '${parsedMonth}-${year}'`,
+                ),
+            },
+        });
+
+        return appointments;
+    }
+
+    public async findAllInDayFromProvider({
+        provider_id,
+        day,
+        month,
+        year,
+    }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+        const parsedDay = String(day).padStart(2, '0');
+        const parsedMonth = String(month).padStart(2, '0');
+
+        const appointments = await this.ormRepository.find({
+            where: {
+                provider_id,
+                date: Raw(
+                    dateFieldName =>
+                        `to_chart(${dateFieldName}.'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
                 ),
             },
         });
