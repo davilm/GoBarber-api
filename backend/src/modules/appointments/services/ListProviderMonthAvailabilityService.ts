1359bf5bc269447fb6f52dc96ec5c37fe22e0419
Davi Leal <lealm.davi@gmail.com>
2020-10-06 18:19:32 -0300

    list availability days

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
-
+import { getDaysInMonth, getDate } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import appointmentsRouter from '../infra/http/routes/appointments.routes';
 
 interface IRequest {
     provider_id: string;
@@ -25,7 +26,7 @@ class ListProviderMonthAvailabilityService {
         month,
         year,
     }: IRequest): Promise<IResponse> {
-        const appointmens = await this.appointmentsRepository.findAllInMonthFromProvider(
+        const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
             {
                 provider_id,
                 year,
@@ -33,7 +34,25 @@ class ListProviderMonthAvailabilityService {
             },
         );
 
-        return [{ day: 1, available: false }];
+        const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+        const eachDayArray = Array.from(
+            { length: numberOfDaysInMonth },
+            (_, index) => index + 1,
+        );
+
+        const availability = eachDayArray.map(day => {
+            const appointmentsInDay = appointments.filter(appointment => {
+                return getDate(appointment.date) === day;
+            });
+
+            return {
+                day,
+                available: appointmentsInDay.length < 10,
+            };
+        });
+
+        return availability;
     }
 }
 
